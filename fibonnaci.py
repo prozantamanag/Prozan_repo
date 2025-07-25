6d2f9bb0cec883dfa1fd6462fb055f7ce9913668
Projan Tamang <prozantamang30@gmail.com>
2025-07-25 09:01:31 +0530

    Adding basic python program

---
+++ fibonnaci.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    sequence = []
+    a, b = 0, 1
+    while len(sequence) < n:
+        sequence.append(a)
+        a, b = b, a + b
+    return sequence
+
+if __name__ == "__main__":
+    count = int(input("How many Fibonacci numbers? "))
+    print(fibonacci(count))
